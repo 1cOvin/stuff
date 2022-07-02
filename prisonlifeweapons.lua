72c5ccb57859492bc9ce487bf0c15ae3786a1abf
Ovin <83101633+1cOvin@users.noreply.github.com>
2022-07-03 01:08:08 +0300

    Update prisonlifeweapons.lua

--- prisonlifeweapons.lua
+++ prisonlifeweapons.lua
@@ -1,3 +1,4 @@
+-- faw
 local n = game:GetService("ReplicatedStorage").ShootEvent
 for i,v in pairs(Workspace.Prison_ITEMS.giver:GetChildren()) do
 local n = Workspace.Remote.ItemHandler:InvokeServer(v.ITEMPICKUP)
